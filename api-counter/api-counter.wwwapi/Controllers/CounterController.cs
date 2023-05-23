a7ae595f1123896353317fb5762a43a481d561b8
uerbzr <nigel.sibbert@gmail.com>
2023-05-23 12:09:33 +0100

    updated exercise.  Has 4 Cores and 2 Extensions. No tests in the solution!

---
+++ api-counter/api-counter.wwwapi/Controllers/CounterController.cs
@@ -0,0 +1,73 @@
+﻿using api_counter.wwwapi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_counter.wwwapi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CounterController : ControllerBase
+    {
+        public static List<Counter> counters = new List<Counter>();
+
+
+        public CounterController()
+        {
+            if (counters.Count == 0)
+            {
+                counters.Add(new Counter() { Id = 1, Name = "Books", Value = 5 });
+                counters.Add(new Counter() { Id = 2, Name = "Toys", Value = 2 });
+                counters.Add(new Counter() { Id = 3, Name = "Videogames", Value = 8 });
+                counters.Add(new Counter() { Id = 4, Name = "Pencils", Value = 3 });
+                counters.Add(new Counter() { Id = 5, Name = "Notepads", Value = 7 });
+            }
+        }
+
+
+        //TODO: 1. write a method that returns all counters in the counters list.  use method below as a starting point
+        [HttpGet]
+        public async Task<IResult> GetAllCounters()
+        {
+            //change the number returned in the line below to counter list variable
+            return Results.Ok(1);
+        }
+
+        //TODO: 2. write a method to return a single counter based on the id being passed in.  complete method below
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IResult> GetACounter()
+        {
+            //write code here replacing the string.Empty
+            var counter = string.Empty;
+
+            //leave return line the same
+            return counter != null ? Results.Ok(counters) : Results.NotFound();
+        }
+
+        //TODO: 3.  write another controlller method that returns counters that have a value greater than the {number} passed in.
+        // use method below as starting point
+        [HttpGet]
+        [Route("greaterthan{number}")]
+        public async Task<IResult> Get(int number)
+        {
+            return Results.Ok(counters.Where(i => i.Value >= number).ToList());
+        }
+
+        ////TODO:  4. write another controlller method that returns counters that have a value less than the {number} passed in.
+      
+      
+
+
+
+        //Extension #1
+        //TODO:  1. Write a controller method that increments the Value property of a counter of any given Id.
+        //e.g.  with an Id=1  the Books counter Value should be increased from 5 to 6
+        //return the counter you have increased
+        
+
+        //Extension #2
+        //TODO: 2. Write a controller method that decrements the Value property of a counter of any given Id.
+        //e.g.  with an Id=1  the Books counter Value should be decreased from 5 to 4
+        //return the counter you have decreased
+    }
+}
---
+++ api-counter/api-counter.wwwapi/Models/Counter.cs
@@ -0,0 +1,10 @@
+﻿namespace api_counter.wwwapi.Models
+{
+    public class Counter
+    {
+        public int Id { get; set; }
+        // @TODO Create int property called Value holding the counter value
+        public int Value { get; set; }
+        public string Name { get; set; }
+    }
+}
